3f3b6921a9d54ae027e9f95952648460b4030720
Rusty Abbott <rusty@hutchinson-island.com>
2018-10-10 10:25:31 -0400

    Link FAB to the search page

--- src/components/FAB.js
+++ src/components/FAB.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default class FAB extends React.Component {
   render() {
     return (
       <div className="open-search">
-        <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+        <Link to="/search">Add a book</Link>
       </div>
     )
   }
